38d0e20f8dccca82db46893ea307b9ac029b3780
Kushal Kattel <kushalkattel0@gmail.com>
2021-08-20 17:03:47 +0545

    first commit

---
+++ meat-data/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from "./Data"
+function App() {
+  return (
+    <div className="App">
+      <Data />
+    </div>
+  );
+}
+
+export default App;
